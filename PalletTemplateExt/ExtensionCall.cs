05ee88e103cd59298ab15b06c4a040177b060d74
darkfriend77 <darkfriend77@users.noreply.github.com>
2021-09-19 18:47:44 +0200

    Initial commit

---
+++ PalletTemplateExt/ExtensionCall.cs
@@ -0,0 +1,29 @@
+﻿using SubstrateNetApi.Model.Types.Base;
+using SubstrateNetApi.Model.Types.Custom;
+using System;
+
+namespace SubstrateNetApi.Model.Calls
+{
+    public class ExtensionCall
+    {
+
+        //{
+        //    name: do_something,
+        //    args:
+        //    [
+        //      {
+        //        name: something,
+        //        type: u32
+        //       }
+        //    ],
+        //    documentation:[
+        //      An example dispatchable that takes a singles value as a parameter, writes the value to,
+        //      storage and emits an event. This function must be dispatched by a signed extrinsic.
+        //    ]
+        //},
+        public static GenericExtrinsicCall DoSomething(U32 something)
+        {
+            return new GenericExtrinsicCall("TemplateModule", "do_something", something);
+        }
+    }
+}
